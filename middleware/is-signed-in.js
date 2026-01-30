38c8a1cf5d2692d3a53234660c55b8c4bf545173
Mu Mawla <cpt.mohd@gmail.com>
2026-01-30 21:23:48 +0300

    docs: Add JSDoc comment to isSignedIn middleware

--- middleware/is-signed-in.js
+++ middleware/is-signed-in.js
@@ -1,7 +1,10 @@
+/**
+ * Authentication middleware
+ * Redirects unauthenticated users to sign-in page
+ */
 const isSignedIn = (req, res, next) => {
   if (req.session.user) return next();
   res.redirect("/auth/sign-in");
 };
 
 module.exports = isSignedIn;
-
