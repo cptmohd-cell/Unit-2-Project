6df0bb334a57a31f174546fc4e9dd37b40dfcedc
Mu Mawla <cpt.mohd@gmail.com>
2026-01-30 21:24:01 +0300

    docs: Add JSDoc comment to passUserToView middleware

--- middleware/pass-user-to-view.js
+++ middleware/pass-user-to-view.js
@@ -1,3 +1,7 @@
+/**
+ * View Helper middleware
+ * Makes user session data available to all EJS templates via res.locals
+ */
 const passUserToView = (req, res, next) => {
   res.locals.user = req.session.user ? req.session.user : null;
   next();
